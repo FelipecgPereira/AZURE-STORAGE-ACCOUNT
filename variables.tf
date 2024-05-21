62805967e3214e0067da26e508390320ab1dcf3b
FelipecgPereira <felipepereiracg@gmail.com>
2024-05-20 22:14:34 -0300

    first commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "location" {
+  description = "value of the location"
+  default     = "East US"
+  type =  string 
+}
+
+variable "account_tier" {
+  description = "value of the account_tier"
+  default     = "Standard"
+  type =  string 
+}
+
+variable "account_replication_type" {
+  description = "value of the account_replication_type"
+  default     = "LRS"
+  type =  string 
+}
